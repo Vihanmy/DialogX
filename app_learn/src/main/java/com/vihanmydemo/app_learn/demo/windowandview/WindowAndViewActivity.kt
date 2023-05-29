9ca020b86a5b77e437d2eb566362794bb19f8e52
vihanmy <vihanmy@gmail.com>
2023-05-29 17:48:33 +0800

    🆕 (app_learn) :WindowAndViewActivity 添加学习案例
    1. 改变导航栏和状态栏占位view的背景色
    2. Activity 的 DecorView 是一个 FrameLayout,可以向DecorView添加View以实现显示弹窗的功能

--- app_learn/src/main/java/com/vihanmydemo/app_learn/demo/windowandview/WindowAndViewActivity.kt
+++ app_learn/src/main/java/com/vihanmydemo/app_learn/demo/windowandview/WindowAndViewActivity.kt
@@ -1,11 +1,13 @@
 package com.vihanmydemo.app_learn.demo.windowandview
 
+import android.graphics.Color
 import android.graphics.PixelFormat
 import android.os.Bundle
 import android.view.Gravity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.WindowManager
+import android.widget.FrameLayout
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.isVisible
@@ -22,9 +24,7 @@ import com.vihanmydemo.app_learn.R
  * ```
  */
 class WindowAndViewActivity : AppCompatActivity() {
-    
     private val TAG = this.javaClass.simpleName
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_window_and_view)
@@ -32,14 +32,17 @@ class WindowAndViewActivity : AppCompatActivity() {
         findViewById<View>(R.id.textView).setOnClickListener {
             addView2ActivityWindow(this)
         }
-
+        findViewById<View>(R.id.textView1).setOnClickListener {
+            addView2RootFrameLayout(this)
+        }
+        findViewById<View>(R.id.textView2).setOnClickListener {
+            changeNavigationBarOrStatusBarBackground(this)
+        }
     }
 }
 
 /**
  * 添加View到Activity所在的Window中, 注意避免 window leak 的问题
- *
- * @param activity
  */
 private fun addView2ActivityWindow(activity: AppCompatActivity) {
     val manager = activity.getSystemService(AppCompatActivity.WINDOW_SERVICE) as WindowManager
@@ -64,4 +67,34 @@ private fun addView2ActivityWindow(activity: AppCompatActivity) {
     })
 
     manager.addView(addedView, layoutParams)
+}
+
+/**
+ * Activity 的 DecorView 是一个 FrameLayout, 可以通过向这个FrameLayout添加View以实现显示弹窗的功能
+ * 添加的区域能够覆盖到状态栏和导航栏区域,但是被导航栏和状态栏覆盖
+ */
+private fun addView2RootFrameLayout(activity: AppCompatActivity) {
+    val addedView = LayoutInflater.from(activity).inflate(R.layout.wav_view_4_window_add, null).apply {
+        setOnClickListener {
+            it.isVisible = false
+            Toast.makeText(activity, "添加的view被点击", Toast.LENGTH_SHORT).show()
+        }
+    }
+    val decorView: FrameLayout? = (activity.window.decorView as? FrameLayout)
+    decorView?.addView(addedView)
+}
+
+/**
+ * 改变导航栏和状态栏占位view的背景色
+ */
+private fun changeNavigationBarOrStatusBarBackground(
+    activity: AppCompatActivity
+) {
+    val decorView = activity.window.decorView
+
+    val statusBarPlaceholderView = decorView.findViewById<View>(android.R.id.statusBarBackground)
+    val navigationBarPlaceholderView = decorView.findViewById<View>(android.R.id.navigationBarBackground)
+
+    statusBarPlaceholderView.setBackgroundColor(Color.MAGENTA)
+    navigationBarPlaceholderView.setBackgroundColor(Color.MAGENTA)
 }
